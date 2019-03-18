913d899b2e10fc042f7a81da14c340cd54cb2317
kevinmcalear <kevin.mcalear@gmail.com>
2019-03-18 18:05:38 -0400

    groupwork

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
+const toyCollectionDiv = document.querySelector('#toy-collection')
 let addToy = false
 
 // YOUR CODE HERE
@@ -10,6 +11,14 @@ addBtn.addEventListener('click', () => {
   if (addToy) {
     toyForm.style.display = 'block'
     // submit listener here
+    toyForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const name = e.target.name.value;
+      const image = e.target.image.value;
+      createNewToy(name, image).then(newToy => {
+        toyCollectionDiv.innerHTML += createToyDiv(newToy)
+      });
+    })
   } else {
     toyForm.style.display = 'none'
   }
@@ -17,3 +26,76 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+const getAndysToys = () => {
+  fetch(`http://localhost:3000/toys`)
+    .then(response => response.json())
+    .then((toys) => {
+      toys.forEach((toy) => {
+        const toyDiv = createToyDiv(toy)
+        toyCollectionDiv.innerHTML += toyDiv
+      })
+    })
+}
+
+const createNewToy = (name, imageURL) => {
+  const settings = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        image: imageURL,
+        likes: 0
+      })
+    }
+    return fetch('http://localhost:3000/toys', settings)
+    .then(response => response.json())
+}
+
+
+getAndysToys()
+
+// document.addEventListener('DOMContentLoaded', getAndysToys)
+
+const createToyDiv = (toyData) => {
+  return (
+    `<div class="card">
+        <h2>${toyData.name}</h2>
+        <img src="${toyData.image}" class="toy-avatar" />
+        <p data-like-count="${toyData.likes}">${toyData.likes} Likes </p>
+        <button data-id="${toyData.id}" class="like-btn">Like <3</button>
+      </div>`
+    )
+}
+
+toyCollectionDiv.addEventListener('click', (e) => {
+  if (e.target.className === 'like-btn') {
+    console.log('i have no clue what to log')
+    const foundCardDiv = e.target.parentElement;
+    const p = foundCardDiv.querySelector('p');
+    const cardId = e.target.dataset.id;
+    const updatedLikeCount = parseInt(p.dataset.likeCount)+1;
+    updateLikes(cardId, updatedLikeCount).then((updatedToy) => {
+      p.innerText = `${updatedToy.likes} Likes `
+      p.dataset.likeCount = updatedToy.likes
+    })
+  }
+})
+
+const updateLikes = (id, number) => {
+  const settings = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        likes: number
+      })
+    }
+    return fetch(`http://localhost:3000/toys/${id}`, settings)
+    .then(response => response.json())
+    // hold off for now
+}
